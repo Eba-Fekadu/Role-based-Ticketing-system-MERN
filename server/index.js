e4ff2a925ce3465fc544631b08ba516728898044
Eba Fekadu <ebafekadu@gmail.com>
2025-03-06 23:01:18 +0300

    render host changes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
+import path from "path"
 
 dotenv.config()
 
@@ -14,9 +15,13 @@ mongoose.connect(process.env.MONGO).then(()=> {
 .catch((err)=>{
     console.log(err)
 })
+const __dirname = path.resolve()
 const app = express()
 app.use(express.json())
 app.use(cookieParser());
+
+
+
 
 app.listen(3000,()=> {
     console.log('Server is running on port 3000')
